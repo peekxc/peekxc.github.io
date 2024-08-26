aac907258f80baec4e6d92b9a2d72b7dd2802223
peekxc <matt.piekenbrock@gmail.com>
2024-08-26 16:57:01 -0400

    Various layout shift improvements, better compact view, (re)-functioning TOC

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,4 @@
-const PRODUCTION = true
+const PRODUCTION = false
 
 // Node-js imports
 import fs from 'fs';
@@ -10,15 +10,18 @@ import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 // Third-party plugins
 import { compress } from 'eleventy-plugin-compress';
 import lazyimages from 'eleventy-plugin-lazyimages';
-import pluginTOC from 'eleventy-plugin-toc';
+// import pluginTOC from 'eleventy-plugin-toc';
+import pluginTOC from 'eleventy-plugin-nesting-toc';
 import markdownIt from "markdown-it"
 import markdownItKatex from "@aquabx/markdown-it-katex";
 import markdownItAnchor from 'markdown-it-anchor';
 import markdownItAttrs from 'markdown-it-attrs';
 import readingTime from 'reading-time';
 import lightningcss from '@11tyrocks/eleventy-plugin-lightningcss';
+import HTMLParser from 'node-html-parser'
 // import { Image } from "@11ty/eleventy-img"; // buggy 
 // import { InputPathToUrlTransformPlugin } from "@11ty/eleventy"; // messes with paths
+import { DateTime } from 'luxon';
 
 // Local plugins 
 import htmlMinifier from "./_11ty/html-minifier.js"
@@ -30,7 +33,8 @@ function readHTML(path) {
 
 function reading_time(content){
 	const stats = readingTime(content, { 'wordsPerMinute': 400 });
-	return stats.text + ", " + stats.words + " words";
+	return stats.text; 
+	// + stats.words + " words"
 }
 
 export default function (config) {
@@ -42,7 +46,6 @@ export default function (config) {
 
 	// Generate table of contents
 	config.addPlugin(syntaxHighlight);
-	config.addPlugin(pluginTOC, { tags: ['h2'] }); // this generates the sidebar needed for scrollspy!
 	
 	// Minify HTML, CSS, images
 	if (PRODUCTION){
@@ -73,12 +76,21 @@ export default function (config) {
 		.use(markdownItAttrs);	
 	config.setLibrary("md", md);
 
+	// Register table of contents plugin 
+	// NOTE: this registers a TOC filter, through in a non-standard way
+	pluginTOC.configFunction(config, { tags: ['h2'] });
+
 	// Add filters to PUG
 	config.addFilter("readingTime", reading_time);
 	config.addFilter("readHTML", readHTML);
 	config.addFilter("markdown", (content) => md.render(content));
 	config.addFilter("markdown_inline", (content) => md.renderInline(content));
 	config.addFilter("uppercase", (string) => string.toUpperCase());
+	config.addFilter("toHTML", (content) => HTMLParser.parse(content).firstChild);
+	config.addFilter("date", (date) => DateTime.fromISO(date).toLocaleString(DateTime.DATE_MED))
+	// HTMLParser.parse(value).firstChild
+	// config.addFilter("toc", (content) => pluginTOC(content));
+	// console.log(pluginTOC)
 
 	// Configure pug plugin (see: https://github.com/11ty/eleventy-plugin-template-languages/blob/main/pug/test/test-plugin-core-tests.js)
 	config.addPlugin(pugPlugin, { 
