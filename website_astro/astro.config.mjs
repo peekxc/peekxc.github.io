454db12022e74d8223f1acb570832ac15303cea0
peekxc <matt.piekenbrock@gmail.com>
2025-10-08 22:34:59 -0400

    Astro version of website

---
+++ website_astro/astro.config.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite" // v4
+// import tailwindcss from "@astrojs/tailwind"
+import mdx from '@astrojs/mdx';
+
+// import smartypants from "remark-smartypants";
+// import remarkRehype from 'remark-rehype'
+// import remarkMath from 'remark-math' /* for latex math support */
+// import rehypeKatex from 'rehype-katex'
+import remarkDirective from 'remark-directive';
+import remarkDirectiveRehype from "remark-directive-rehype";
+// import remarkFrontmatter from 'remark-frontmatter'
+// import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+
+import preact from '@astrojs/preact';
+// remarkDirective, remarkDirectiveRehype, 
+// https://astro.build/config
+export default defineConfig({
+  site: "https://mattpiekenbrock.com",
+  trailingSlash: "never",
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en"
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    smartypants: true,
+    // remarkPlugins: [remarkDirective, remarkFrontmatter],
+  },
+  integrations: [
+    mdx({
+      smartypants: true, 
+      remarkPlugins: [remarkDirective, remarkDirectiveRehype],
+      rehypePlugins: [],
+      gfm: true, 
+    }), 
+    preact()
+  ]
+});
---
+++ website_astro/content.config.ts
@@ -0,0 +1,11 @@
+// import { defineCollection, z } from 'astro:content';
+// import { glob, file } from 'astro/loaders';
+
+// // 3. Define your collection(s)
+// const jobs = defineCollection({ 
+// 	loader: glob({ pattern: "**/*.md", base: "./src/data/blog" }),
+// });
+// const dogs = defineCollection({ /* ... */ });
+
+// // 4. Export a single `collections` object to register your collection(s)
+// export const collections = { blog, dogs };
