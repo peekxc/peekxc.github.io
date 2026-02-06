62977d0f4ddcd537f504fff621e75aa2e5d16ef5
Matt Piekenbrock <matt.piekenbrock@gmail.com>
2026-02-06 17:07:10 -0500

    Updatign the scrollspy, Authorblock, and backgroudn animation components.

--- website_astro/uno.config.ts
+++ website_astro/uno.config.ts
@@ -12,7 +12,9 @@ export default defineConfig({
     'card-base': 'rounded-2xl shadow p-4 bg-white',
     'flex-center': 'flex items-center justify-center',
 		'tag': 'span text-black text-xs px-2 rounded-sm mr-1 min-w-0 py-0.5 bg-slate-100/80 text-slate-600 border border-slate-200/50',
-		'icon-round': 'py-1 px-2 min-w-0 text-xs text-black bg-neutral-300 rounded-xs flex gap-2 text-center'
+		'icon-round': 'py-1 px-2 min-w-0 text-xs text-black bg-neutral-300 rounded-xs flex gap-2 text-center', 
+    'three-col-main': 'col-span-10 lg:col-span-8 2xl:col-span-6',
+    'three-col-side': 'col-span-1 lg:col-span-2 2xl:col-span-3'
   },
 	rules: [
 		['tag', {'white-space': 'nowrap', 'padding-top': '0.15em', 'padding-bottom': '0.15em', }]
